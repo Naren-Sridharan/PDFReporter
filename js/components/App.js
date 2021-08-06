f0bab3da9be2fa52ddf84a06f057a95ad0e4b2ac
Naren-Jegan <naren.jegan26@gmail.com>
2021-08-06 12:50:04 +1000

    Simplified

--- js/components/App.js
+++ js/components/App.js
@@ -1,16 +1,36 @@
 import { neverland as $, html, useState } from "neverland";
-import Router from "./Router";
+import { printPdf } from "./PDFDownloader";
+import { StackedBarChart } from "./Charts";
 
 const App = $(function () {
-	let page = new URLSearchParams(window.location.search).get("p");
-
 	const [auth, setAuth] = useState({
-		page: page,
+		pagenumber: 0,
 	});
 
-	console.log("Reached app");
+	const pages = ["General", "cantril", "GAD-2", "PHQ-2", "CHS", "CSI-4"];
+	console.log(pages.length - 1);
 
-	return html`${Router(auth, setAuth)}`;
+	return html` <div id="${pages[auth.pagenumber]}" class="page">
+		<h1>Mental Health Report for Resilient Youth Australia</h1>
+		<h2>${pages[auth.pagenumber]}</h2>
+		${StackedBarChart(pages[auth.pagenumber])}
+		<button
+			id="prev"
+			onclick="${() => setAuth({ pagenumber: auth.pagenumber - 1 })}"
+			disabled="${auth.pagenumber == 0}"
+		>
+			Previous
+		</button>
+		<button id="print_report" onclick="${printPdf}">Print PDF Report</button>
+		<button
+			id="next"
+			onclick="${() => setAuth({ pagenumber: auth.pagenumber + 1 })}"
+			label="Next"
+			disabled="${auth.pagenumber == pages.length - 1}"
+		>
+			Next
+		</button>
+	</div>`;
 });
 
 export default App;
